797b1969a24ecf758ed0d8b6bd16beb5501a0857
Pawandeep Singh <pawandeep20@gmail.com>
2021-01-05 22:43:59 -0500

    test1

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyHomePage extends StatelessWidget {
           child: Column(
             children: [
                             
-              Text('hello'),
+              Text('hellooooooo Trump IT'),
               FlatButton(
                 child: Text('Click me to test'),
                 onPressed: () {
